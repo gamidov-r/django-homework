58b36c995e981c0d9f4599e506dfb6fdabd6bfed
ruslan <gamidov.r@mail.ru>
2026-01-19 23:26:30 +0300

    fix README, .gitignore

--- catalog/urls.py
+++ catalog/urls.py
@@ -2,10 +2,9 @@ from django.urls import path
 from catalog.apps import NewappConfig
 from catalog.views import home, contacts
 
-
 app_name = NewappConfig.name
 
 urlpatterns = [
-    path('',home, name='home'),
-    path('contacts/', contacts, name='contacts'),
+    path("", home, name="home"),
+    path("contacts/", contacts, name="contacts"),
 ]
