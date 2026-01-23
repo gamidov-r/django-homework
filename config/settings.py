1f779eee483803b31b78d5343672366c0303187e
ruslan <gamidov.r@mail.ru>
2026-01-23 18:52:22 +0300

    add MEDIA_ROOT and MEDIA_URL in settings.py, include in config/urls.py. fix custom cmd add_test_product

--- config/settings.py
+++ config/settings.py
@@ -114,3 +114,9 @@ USE_TZ = True
 STATIC_URL = "static/"
 
 STATICFILES_DIRS = (BASE_DIR / "static",)
+
+DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+MEDIA_URL = "/media/"
+
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
